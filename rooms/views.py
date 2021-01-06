ed10aea08e0b253569dfdc480a950e22605fd780
root <root@DESKTOP-6TESVTH.localdomain>
2021-01-06 23:13:48 +0900

    #10 INTRODUCTION TO VIEWS AND URLS

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+# from django.http import HttpResponse
+
+
+def all_rooms(request):
+    # now = datetime.now()
+    # hungry = True
+    # return HttpResponse(context="hello")
+
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
