927bc2ce5f54ddecbbccb827e23db0c82e597d5d
root <root@DESKTOP-6TESVTH.localdomain>
2021-01-07 15:08:54 +0900

    #11 HOMEVIEW

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
-# from django.http import HttpResponse
 
+class HomeView(ListView):
 
-def all_rooms(request):
-    # now = datetime.now()
-    # hungry = True
-    # return HttpResponse(context="hello")
+    """ HomeView Definition """
 
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
