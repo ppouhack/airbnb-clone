a4991c166859716ff25faf3f67bb8219feeb6877
root <root@DESKTOP-6TESVTH.localdomain>
2021-01-10 15:40:48 +0900

    #12 DETAILVIEW FBV

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "uurhvv)wqln%7e#l0gu_&+&0s5*21)%i3-bp5-lu5z-=^155)!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+# DEBUG = True
+DEBUG = False
+
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 
 # Application definition
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,8 @@
+from django.urls import reverse
+from django.http import Http404
 from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -18,3 +21,12 @@ class HomeView(ListView):
         now = timezone.now()
         context["now"] = now
         return context
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
+        # return redirect(reverse("core:home"))
