cafb2830283e6cb162ffb9109853c053809b9fc9
root <root@DESKTOP-6TESVTH.localdomain>
2021-02-13 00:37:21 +0900

    #21 User Profile Placeholder

--- users/views.py
+++ users/views.py
@@ -74,6 +74,12 @@ class UpdateProfileView(UpdateView):
     def get_object(self, queryset=None):
         return self.request.user
 
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["birthdate"].widget.attrs = {"placeholder": "Birthdate"}
+        form.fields["first_name"].widget.attrs = {"placeholder": "First name"}
+        return form
+
     # def form_valid(self, form):
     #     email = form.cleaned_data.get("email")
     #     self.object.username = email
@@ -83,3 +89,12 @@ class UpdateProfileView(UpdateView):
 
 class UpdatePasswordView(PasswordChangeView):
     template_name = "users/update_password.html"
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["old_password"].widget.attrs = {"placeholder": "Current password"}
+        form.fields["new_password1"].widget.attrs = {"placeholder": "New Password"}
+        form.fields["new_password2"].widget.attrs = {
+            "placeholder": "Confirm new password"
+        }
+        return form
