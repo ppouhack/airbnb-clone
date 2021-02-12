71ae3e6eb2e964b343da1e6af990b9f108422ff0
root <root@DESKTOP-6TESVTH.localdomain>
2021-02-12 20:24:30 +0900

    #21 User Profile UpdateView

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login"),
-    path("logout", views.log_out, name="logout"),
-    path("signup", views.SignUpView.as_view(), name="signup"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.log_out, name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
 ]
